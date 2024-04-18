7d7523146ee22165d0666e4662d198f6511d2662
Erik Golinelli <erik@codekraft.it>
2024-04-18 12:04:08 +0200

    header

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -40,7 +40,6 @@ export default function Header({
 				<select
 					value={index}
 					id={"select-demo-page"}
-					name="select-demo-page"
 					className={
 						"bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
 					}
